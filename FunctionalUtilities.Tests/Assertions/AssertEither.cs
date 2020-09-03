610a70b6815e9c21ec1c6f51620ed5a9eeb2135b
Matt <matt_bristow@rocketmail.com>
2020-09-03 14:26:48 +0100

    Updated custom assertions

--- FunctionalUtilities.Tests/Assertions/AssertEither.cs
+++ FunctionalUtilities.Tests/Assertions/AssertEither.cs
@@ -1,13 +1,23 @@
-﻿namespace FunctionalUtilities.Tests
+﻿using Xunit;
+
+namespace FunctionalUtilities.Tests
 {
     public static class AssertEither
     {
-        public static void Left<TLeft, TRight>(Either<TLeft, TRight> either) =>
+        public static void Left<TLeft, TRight>(Either<TLeft, TRight> either)
+        {
+            Assert.NotNull(either);
+
             either.ReduceLeft(
                 reducer: x => throw new IsRightException(x));
+        }
 
-        public static void Right<TLeft, TRight>(Either<TLeft, TRight> either) =>
+        public static void Right<TLeft, TRight>(Either<TLeft, TRight> either)
+        {
+            Assert.NotNull(either);
+
             either.ReduceRight(
                 reducer: x => throw new IsLeftException(x));
+        }
     }
 }
--- FunctionalUtilities.Tests/Assertions/AssertMaybe.cs
+++ FunctionalUtilities.Tests/Assertions/AssertMaybe.cs
@@ -1,9 +1,19 @@
-﻿namespace FunctionalUtilities.Tests
+﻿using Xunit;
+
+namespace FunctionalUtilities.Tests
 {
     public static class AssertMaybe
     {
-        public static void Some<T>(Maybe<T> maybe) => maybe.Reduce(() => throw new IsNoneException());
+        public static void Some<T>(Maybe<T> maybe)
+        {
+            Assert.NotNull(maybe);
+            maybe.Reduce(() => throw new IsNoneException());
+        }
 
-        public static void None<T>(Maybe<T> maybe) => maybe.Do(x => throw new IsSomeException(x));
+        public static void None<T>(Maybe<T> maybe)
+        {
+            Assert.NotNull(maybe);
+            maybe.Do(x => throw new IsSomeException(x));
+        }
     }
 }
