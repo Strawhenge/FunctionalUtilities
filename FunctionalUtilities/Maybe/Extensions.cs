b666ebf262b4411aa58c269221204e2f26282096
Matt <matt_bristow@rocketmail.com>
2020-09-03 14:44:49 +0100

    Updated Maybe extensions

--- FunctionalUtilities/Maybe/Extensions.cs
+++ FunctionalUtilities/Maybe/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using FunctionalUtilities;
+using System;
 
 public static class MaybeExtensions
 {
@@ -11,4 +12,11 @@ public static class MaybeExtensions
         : Maybe.None<T>();
 
     public static Maybe<T> Flatten<T>(this Maybe<Maybe<T>> maybe) => maybe.Reduce(() => Maybe.None<T>());
+
+    public static bool HasValue<T>(this Maybe<T> maybe) => maybe
+        .Map(_ => true)
+        .Reduce(() => false);
+
+    public static T ReduceUnsafe<T>(this Maybe<T> maybe) => maybe.Reduce(
+        () => throw new InvalidOperationException($"Cannot reduce value from {maybe.GetType().Name}"));
 }
