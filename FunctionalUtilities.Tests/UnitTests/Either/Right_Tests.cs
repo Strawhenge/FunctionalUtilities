4fdf459bdc4e7b9ca919460d7ca861cff0153195
Matt <matt_bristow@rocketmail.com>
2020-09-03 13:42:57 +0100

    Added more tests

---
+++ FunctionalUtilities.Tests/UnitTests/Either/Right_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace FunctionalUtilities.Tests.UnitTests
+{
+    public class Right_Tests
+    {
+        public void ReduceRight_ShouldReturnExpectedString()
+        {
+            var expected = "This is the string.";
+
+            var subject = Either.Right<object, string>(expected);
+
+            var actual = subject.ReduceRight(
+                _ => throw new Exception("Unexpected reducer call."));
+
+            Assert.NotNull(actual);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
