6f77709e70649c66cdf2027723921e0e81f4338b
Matt <matt_bristow@rocketmail.com>
2020-09-03 13:31:53 +0100

    Added more tests

---
+++ FunctionalUtilities.Tests/UnitTests/Maybe/Some_Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xunit;
+
+namespace FunctionalUtilities.Tests.UnitTests
+{
+    public class Some_Tests
+    {
+        [Fact]
+        public void Reduce_ShouldReturnExpected()
+        {
+            var expected = "This is the string.";
+
+            var subject = Maybe.Some(expected);
+
+            var actual = subject.Reduce(
+                reducer: () => throw new Exception("Unexpected call to reducer"));
+
+            Assert.NotNull(actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Do_ShouldInvokeAction()
+        {
+            bool hasInvoked = false;
+
+            var subject = Maybe.Some(new object());
+
+            subject.Do(_ => hasInvoked = true);
+
+            Assert.True(hasInvoked);
+        }
+
+        [Fact]
+        public void Map_ThenReduce_ShouldReturnMappingResult()
+        {
+            var expected = new object();
+
+            var subject = Maybe.Some("This is a string.");
+
+            var actual = subject
+                .Map(_ => expected)
+                .Reduce(reducer: () => throw new Exception("Unexpected call to reducer"));
+
+            Assert.NotNull(actual);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
