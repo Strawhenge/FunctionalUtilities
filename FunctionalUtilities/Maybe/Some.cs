27ac2f52a6f5812e2e34a17394c008ad83321dfe
Matt Bristow <strawhengestudios@gmail.com>
2022-10-15 23:14:42 +0100

    fix warnings

--- FunctionalUtilities/Maybe/Some.cs
+++ FunctionalUtilities/Maybe/Some.cs
@@ -4,19 +4,19 @@ namespace FunctionalUtilities
 {
     sealed class Some<T> : Maybe<T>
     {
-        readonly T value;
+        readonly T _value;
 
         public Some(T value)
         {
             if (value == null) throw new ArgumentNullException(nameof(value));
 
-            this.value = value;
+            _value = value;
         }
 
-        public override void Do(Action<T> action) => action(value);
+        public override void Do(Action<T> action) => action(_value);
 
-        public override Maybe<TNew> Map<TNew>(Func<T, TNew> mapping) => new Some<TNew>(mapping(value));
+        public override Maybe<TNew> Map<TNew>(Func<T, TNew> mapping) => new Some<TNew>(mapping(_value));
 
-        public override T Reduce(Func<T> fallback) => value;
+        public override T Reduce(Func<T> fallback) => _value;
     }
 }
