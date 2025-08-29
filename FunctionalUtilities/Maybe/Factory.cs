7b4e1ccf00e29d32dd7782dc8eff642ad4487e0e
Strawhenge <strawhengestudios@gmail.com>
2025-08-29 22:59:54 +0100

    added FromNullable method

--- FunctionalUtilities/Maybe/Factory.cs
+++ FunctionalUtilities/Maybe/Factory.cs
@@ -5,5 +5,9 @@
         public static Maybe<T> Some<T>(T value) => new Some<T>(value);
 
         public static Maybe<T> None<T>() => FunctionalUtilities.None<T>.Instance;
+
+        public static Maybe<T> FromNullable<T>(T value) => ReferenceEquals(value, null)
+            ? None<T>()
+            : Some(value);
     }
 }
